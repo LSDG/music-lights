80463a54a11d8ea1ba856f78ab6343b45ea87850
David H. Bronke <whitelynx@gmail.com>
2012-12-10 20:16:15 -0600

    Rewrote most of the main app to use PyGame instead of PyAudio, and split lots of things into classes.
    Now, hopefully it'll run on the Raspberry Pi.

--- player.py
+++ player.py
@@ -1,38 +1,12 @@
 #!/bin/env python
 from __future__ import print_function
-import time
-from multiprocessing import Process, Queue
-
-pins = [0, 1, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23, 24, 25]
-
-
-def launchProc(q):
-    import RPi.GPIO as GPIO
-
-    GPIO.setmode(GPIO.BCM)
-    for pin in pins:
-        GPIO.setup(pin, GPIO.OUT)
-
-    while True:
-        msg = q.get(True)
-
-        if msg == 'end':
-            break
-
-        for channel, value in enumerate(msg):
-            GPIO.output(pins[channel], value)
-
-q = Queue()
-p = Process(target=launchProc, args=(q,))
-p.start()
-
 import ConfigParser
 import collections
 import datetime
+from itertools import cycle
+from multiprocessing import Process, Queue
 import os
 import sys
-
-from itertools import cycle
 
 from numpy import array_split, short, fromstring
 from numpy.random import rand as rand_array
@@ -41,10 +15,39 @@ import anfft
 
 import audioread
 
-import pyaudio
+import pygame
+import pygame.locals
 
 import ansi
 
+
+pins = [0, 1, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23, 24, 25]
+
+DONE_PLAYING_CHUNK = pygame.locals.USEREVENT
+
+STOP = 0
+CONTINUE = 1
+
+
+def launchProc(messageQueue):
+    import RPi.GPIO as GPIO
+
+    GPIO.setmode(GPIO.BCM)
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+
+    while True:
+        msg = messageQueue.get(True)
+
+        if msg == 'end':
+            break
+
+        for channel, value in enumerate(msg):
+            GPIO.output(pins[channel], value)
+
+messageQueue = Queue()
+p = Process(target=launchProc, args=(messageQueue, ))
+p.start()
 
 gcp = ConfigParser.SafeConfigParser()
 gcp.read('config.ini')
@@ -62,126 +65,213 @@ defaultThresholds = map(float, gcp.get('spectrum', 'thresholds').split(','))
 defaultOrder = map(int, gcp.get('spectrum', 'channelOrder').split(','))
 
 
-def statusChar(status):
-    if status == pyaudio.paOutputOverflow:
-        return "\033[1;33mO\033[m"
-    elif status == pyaudio.paOutputUnderflow:
-        return "\033[1;31mU\033[m"
-    else:
-        return "."
-
-
 files = sys.argv[1:]
+
+
+# Initialize pygame.mixer
+pygame.mixer.pre_init(frequency=44100)
+pygame.init()
 
 # Warm up ANFFT, allowing it to determine which FFT algorithm will work fastest on this machine.
 anfft.fft(rand_array(1024), measure=True)
 
-totalFramesRead = 0.0
-recentFrameStatuses = collections.deque(' ' * 64, maxlen=64)
-lastCallTime = datetime.datetime.now()
 
-# Instantiate PyAudio.
-audio = pyaudio.PyAudio()
+queuedCallbacks = collections.deque()
+eventHandlers = dict()
 
-def playFile(filename):
-    with audioread.audio_open(filename) as inFile:
-        thresholds = defaultThresholds
-        order = defaultOrder
-        if os.path.exists(filename + '.ini'):
+
+class SampleGen(object):
+    def __init__(self, filenames, **kwargs):
+        self.framesPerChunk = kwargs.get('framesPerChunk', 4096)
+
+        self.onSongChanged = set()
+        self.onStopped = set()
+        self.onSample = set()
+
+        self.totalFramesRead = 0.0
+        self.filenameIter = iter(filenames)
+        self.currentData = None
+        self.sampleIter = None
+        self._dataSinceLastSpectrum = []
+
+    def _loadNextFile(self):
+        self.currentFilename = next(self.filenameIter)
+
+        queuedCallbacks.extend(self.onSongChanged)
+
+        self.file = audioread.audio_open(self.currentFilename)
+        self.sampleIter = self.file.read_data(self.framesPerChunk * self.file.channels * bytes_per_frame_per_channel)
+
+    @property
+    def spectrum(self):
+        if self._currentSpectrum is None:
+            dataArr = fromstring(self.currentData, dtype=short)
+            normalized = dataArr / float(2 ** (bytes_per_frame_per_channel * 8))
+
+            # Cut the spectrum down to the appropriate number of bands.
+            bands = array_split(abs(anfft.rfft(normalized)), frequencyBands)
+
+            self._currentSpectrum = map((lambda arr: sum(arr) / len(arr)), bands)
+
+            self._dataSinceLastSpectrum = []
+
+        return self._currentSpectrum
+
+    @property
+    def elapsedTime(self):
+        return self.totalFramesRead / self.file.samplerate
+
+    @property
+    def channels(self):
+        return self.file.channels
+
+    @property
+    def samplerate(self):
+        return self.file.samplerate
+
+    @property
+    def duration(self):
+        return self.file.duration
+
+    def nextChunk(self):
+        if self.sampleIter is None:
+            self._loadNextFile()
+
+        self.currentData = buffer(next(self.sampleIter))
+        #FIXME: Detect end of song, and call self._loadNextFile()!
+
+        self.totalFramesRead += self.framesPerChunk
+        self._currentSpectrum = None
+        self._dataSinceLastSpectrum.append(self.currentData)
+
+        queuedCallbacks.extend(self.onSample)
+
+        return pygame.mixer.Sound(self.currentData)
+
+    def close(self):
+        # Stop stream.
+        self.fFile.close()
+
+
+class SpectrumLightController(object):
+    def __init__(self, sampleGen):
+        self.sampleGen = sampleGen
+        self.lastLightUpdate = datetime.datetime.now()
+
+        sampleGen.onSample.add(self._onSample)
+        sampleGen.onSongChanged.add(self._onSongChanged)
+
+    def _onSongChanged(self):
+        self.frequencyThresholds = defaultThresholds
+        self.frequencyBandOrder = defaultOrder
+
+        iniPath = self.currentFilename + '.ini'
+        if os.path.exists(iniPath):
             cp = ConfigParser.SafeConfigParser()
-            cp.read([filename + '.ini'])
-            thresholds = map(float, cp.get('spectrum', 'thresholds').split(','))
-            order = map(int, cp.get('spectrum', 'channelOrder').split(','))
+            cp.read([iniPath])
+            self.frequencyThresholds = map(float, cp.get('spectrum', 'thresholds').split(','))
+            self.frequencyBandOrder = map(int, cp.get('spectrum', 'channelOrder').split(','))
 
-        global inFileIter, abort
-        inFileIter = None
-        abort = False
+    def _onSample(self):
+        now = datetime.datetime.now()
+        if (now - self.lastLightUpdate).total_seconds() > delayBetweenUpdates:
+            self.lastLightUpdate = now
 
-        def callback(in_data, frame_count, time_info, status):
-            global inFileIter, lastCallTime, port2, totalFramesRead, recentFrameStatuses, abort
+            spectrum = self.sampleGen.spectrum
+            bands = [spectrum[i] for i in self.frequencyBandOrder]
+            lightStates = [level > self.frequencyThresholds[channel] for channel, level in enumerate(bands)]
 
-            if abort:
-                return (None, pyaudio.paAbort)
+            messageQueue.put(lightStates)
 
-            if not inFileIter:
-                inFileIter = inFile.read_data(frame_count * inFile.channels * bytes_per_frame_per_channel)
 
-            data = next(inFileIter)
+class SampleOutput(object):
+    def __init__(self, sampleGen):
+        self.sampleGen = sampleGen
 
-            totalFramesRead += frame_count
+        self.channel = pygame.mixer.Channel(0)
+        self.channel.set_endevent(DONE_PLAYING_CHUNK)
 
-            recentFrameStatuses.appendleft(statusChar(status))
+        self.queueNextSound()  # Start playing the first chunk.
+        self.queueNextSound()  # Queue the next chunk.
 
-            ansi.stdout("{cursor.col.0}{clear.line.all}Current time:"
-                        " {style.bold}{elapsedTime: >7.2f}{style.none} / {totalTime: <7.2f}"
-                        " {style.bold.fg.black}[{style.none}{status}{style.bold.fg.black}]{style.none}",
-                    elapsedTime=totalFramesRead / inFile.samplerate, totalTime=inFile.duration,
-                    status=''.join(recentFrameStatuses), suppressNewline=True)
+        eventHandlers[DONE_PLAYING_CHUNK] = self.queueNextSound
 
-            thisCallTime = datetime.datetime.now()
-            if (thisCallTime - lastCallTime).total_seconds() > delayBetweenUpdates:
-                lastCallTime = thisCallTime
-                dataArr = fromstring(data, dtype=short)
-                normalized = dataArr / float(2 ** (bytes_per_frame_per_channel * 8))
+    def queueNextSound(self, event=None):
+        chunk = sampleGen.nextChunk()
+        chunk.play()
 
-                # Cut the spectrum down to the appropriate number of bands.
-                bands = array_split(abs(anfft.rfft(normalized)), frequencyBands)
-                bands = [bands[i] for i in order]
-
-                spectrum = map((lambda arr: sum(arr) / len(arr)), bands)
-
-                lightStates = [level > thresholds[channel] for channel, level in enumerate(spectrum)]
-
-                q.put(lightStates)
-
-            return (data, pyaudio.paContinue)
-
-        print()
-        ansi.stdout("Playing audio file: {style.fg.blue}{}{style.none}", filename)
-        ansi.stdout("{style.bold.fg.black}channels:{style.none} {inFile.channels}"
-                    "   {style.bold.fg.black}sample rate:{style.none} {inFile.samplerate}"
-                    "   {style.bold.fg.black}duration:{style.none} {inFile.duration}",
-                inFile=inFile)
-        #print("Playing audio file: \033[34m{}\033[m".format(filename))
-        #print("\033[1;30mchannels:\033[m {}   \033[1;30msample rate:\033[m {}   \033[1;30mduration:\033[m {}".format(
-                #inFile.channels, inFile.samplerate, inFile.duration))
-
-        stream = audio.open(
-                format=pyaudio.paInt16,
-                channels=inFile.channels,
-                rate=inFile.samplerate,
-                output=True,
-                stream_callback=callback
+        ansi.stdout(
+                "{cursor.col.0}{clear.line.all}Current time:"
+                    " {style.bold}{file.elapsedTime: >7.2f}{style.none} / {file.duration: <7.2f}",
+                file=sampleGen,
+                suppressNewline=True
                 )
 
-        # Wait for stream to finish.
-        try:
-            while stream.is_active():
-                time.sleep(0.01)
-        except KeyboardInterrupt:
-            print()
-            print("User interrupted; stopping.")
-            abort = True
-            time.sleep(0.2)
 
-        print()
-        print("Stopping audio...")
+def displayFileStarted():
+    print()
+    ansi.stdout(
+            "Playing audio file: {style.fg.blue}{file.currentFilename}{style.none}\n"
+                "{style.bold.fg.black}channels:{style.none} {file.channels}"
+                "   {style.bold.fg.black}sample rate:{style.none} {file.samplerate} Hz"
+                "   {style.bold.fg.black}duration:{style.none} {file.duration} s",
+            file=sampleGen
+            )
 
-        # Stop stream.
-        stream.stop_stream()
-        stream.close()
-        inFile.close()
 
+sampleGen = SampleGen(cycle(files))
+sampleGen.onSongChanged.add(displayFileStarted)
+
+output = SampleOutput(sampleGen)
+
+
+class QuitApplication(Exception):
+    pass
+
+
+def quitApp(event):
+    raise QuitApplication
+
+eventHandlers[pygame.locals.QUIT] = quitApp
+
+
+def unhandledEvent(event):
+    ansi.warn("Unhandled event! {!r}", event)
+
+
+def processEvent(event):
+    if event.type == pygame.locals.NOEVENT:
+        return
+
+    handler = eventHandlers.get(event.type, unhandledEvent)
+    handler(event)
+
+
+# Wait for stream to finish.
 try:
-    for filename in cycle(files):
-        playFile(filename)
+    while True:
+        processEvent(pygame.event.wait())
+
+        # Process any queued callbacks.
+        while queuedCallbacks:
+            callback = queuedCallbacks.popleft()
+            callback()
+
+            # Process waiting events before moving on to the next callback.
+            for event in pygame.event.get():
+                processEvent(event)
+
+except QuitApplication:
+    print()
+    print("Exiting application.")
+
 except KeyboardInterrupt:
     print()
-    print("User interrupted; outer loop stopping")
+    print("User interrupted; exiting.")
 
-    q.put('end')
+ansi.info("Shutting down...")
 
-    # Close PyAudio.
-    audio.terminate()
+messageQueue.put('end')
+pygame.quit()
 
-    ansi.done()
+ansi.done()
