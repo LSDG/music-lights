a268c352ff08dc65cc79dee2a84f36aae48ede4b
David H. Bronke <dbronke@armt.com>
2013-11-04 18:54:18 -0600

    Added docstring to ConfigParserDefault.get_def(), and updated to also return the default if the given section is missing.

--- ConfigParserDefault.py
+++ ConfigParserDefault.py
@@ -3,7 +3,11 @@ import ConfigParser
 
 class ConfigParserDefault(ConfigParser.SafeConfigParser):
     def get_def(self, section, key, default=None):
+        """Get an option value from the named section, returning the given default if either the option or section is
+        not present.
+
+        """
         try:
             return self.get(section, key)
-        except ConfigParser.NoOptionError:
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
             return default
