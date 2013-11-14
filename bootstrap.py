f1006ae59fc72196adf954fa98fb0208fa16dafa
David H. Bronke <dbronke@armt.com>
2013-11-14 16:41:33 -0600

    Split the dependency installation part of the bootstrap script into its own installDeps.py file, and reworked it so it just symlinks any dependencies that are already installed in the host environment.

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ## WARNING: This file is generated
-#!/usr/bin/env python2
+#!/usr/bin/env python
 """Create a "virtual" Python installation
 """
 
@@ -1809,12 +1809,56 @@ def create_bootstrap_script(extra_text, python_version=''):
                + content)
     return content.replace('##EXT' 'END##', extra_text)
 
-
+import importlib
+import os
+from os.path import basename, dirname, join
 import subprocess
+import sys
+
+
+requiredPackages = {
+        'audioread': 'audioread',
+        'hsaudiotag': 'hsaudiotag',
+        'numpy': 'numpy',
+        'pyFFTW': 'pyfftw',
+        'pyalsaaudio': 'alsaaudio',
+        'pyserial': 'serial',
+        }
+
+packageFilesToLink = []
+packagesToInstall = []
+
+for pkg, mod in requiredPackages.items():
+    try:
+        modPath = importlib.import_module(mod).__file__
+        packageFilesToLink.append(modPath)
+        print("Package {!r} already installed; symlinking.".format(pkg))
+
+    except ImportError:
+        packagesToInstall.append(pkg)
+        print("Package {!r} not installed; installing.".format(pkg))
+
 
 def after_install(options, virt_env_dir):
-    requiredPackages = ['audioread', 'hsaudiotag', 'numpy', 'pyFFTW', 'pyalsaaudio', 'pyserial']
-    subprocess.call([join(virt_env_dir, 'bin', 'easy_install')] + requiredPackages)
+    # Symlink packages that are already installed on the system.
+    py_version = 'python%s.%s' % (sys.version_info[0], sys.version_info[1])
+    site_packages_dir = join(virt_env_dir, 'lib', py_version)
+
+    for path in packageFilesToLink:
+        while basename(dirname(path)) != 'site-packages':
+            path = dirname(path)
+
+        try:
+            os.symlink(path, join(site_packages_dir, basename(path)))
+        except Exception:
+            import traceback
+            print("Error symlinking {!r} into {!r}:".format(path, site_packages_dir))
+            traceback.print_exc()
+            sys.exit(1)
+
+    # Install remaining packages
+    subprocess.call([join(virt_env_dir, 'bin', 'easy_install')] + packagesToInstall)
+
 
 def adjust_options(options, args):
     if len(args) == 0:
